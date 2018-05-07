7b741eaf759849bcaac96de193a70dcb5e1a4683
Gaby Salguero <00353817@uca.edu.sv>
2018-05-07 15:02:15 -0600

    Clase cliente relacionada con clase reserva de fecha

--- src/parcialpoo_2x2/Cliente.java
+++ src/parcialpoo_2x2/Cliente.java
@@ -1,82 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package parcialpoo_2x2;
 
+//import java.util.Scanner;
+
 /**
- * Clase que represrant a un clientwe
+ *
  * @author andreu
  */
+
 public class Cliente {
-
     public String nombre;
-    private String telefno;
-    private String DUI;
-    public String numerodeHabitacion;
-    public String piso;
-    private int tarjetaDeCredito;
+    private String dui;Scanner input=new Scanner(System.in);
+    private Reserva fecha;
+    private Administrador1 registra;
 
-    /**
-     * Sirve para
-     * @param nombre
-     * @param telefno
-     * @param DUI
-     * @param numerodeHabitacion
-     * @param piso
-     * @param tarjetaDeCredito 
-     */
-    public Cliente(String nombre, String telefno, String DUI, String numerodeHabitacion, String piso, int tarjetaDeCredito) {
+    public Cliente(String nombre, String dui, Reserva fecha, Habitacion habitacion, Administrador1 registra) {
         this.nombre = nombre;
-        this.telefno = telefno;
-        this.DUI = DUI;
-        this.numerodeHabitacion = numerodeHabitacion;
-        this.piso = piso;
-        this.tarjetaDeCredito = tarjetaDeCredito;
+        this.dui = dui;
+        this.fecha = fecha;
+        this.habitacion = habitacion;
+        this.registra = registra;
+    }
+
+    Cliente() {
+    }
+  
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getNombre() {
         return nombre;
     }
+    
+    public Habitacion habitacion;
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public Habitacion getHabitacion() {
+        return habitacion;
     }
 
-    public String getTelefno() {
-        return telefno;
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
     }
 
-    public void setTelefno(String telefno) {
-        this.telefno = telefno;
+    public Administrador1 getRegistra() {
+        return registra;
     }
 
-    public String getDUI() {
-        return DUI;
+    public void setRegistra(Administrador1 registra) {
+        this.registra = registra;
     }
 
-    public void setDUI(String DUI) {
-        this.DUI = DUI;
+    public Reserva getFecha() {
+        return fecha;
     }
 
-    public String getNumerodeHabitacion() {
-        return numerodeHabitacion;
+    public void setFecha(Reserva fecha) {
+        Reserva reserva = new Reserva();
+        
+        while(true){
+            reserva.Registrarentrada(0, 0, 0);
+            break;
+        }
+        
+        reserva.Registrarsalida(0, 0, 0);
+        reserva.det_estacion(0);
+        this.fecha = fecha;
+    }
+    
+    public String getDui() {
+        return dui;
     }
 
-    public void setNumerodeHabitacion(String numerodeHabitacion) {
-        this.numerodeHabitacion = numerodeHabitacion;
+    public void setDui(String dui) {
+        this.dui = dui;
     }
-
-    public String getPiso() {
-        return piso;
-    }
-
-    public void setPiso(String piso) {
-        this.piso = piso;
-    }
-
-    public int getTarjetaDeCredito() {
-        return tarjetaDeCredito;
-    }
-
-    public void setTarjetaDeCredito(int tarjetaDeCredito) {
-        this.tarjetaDeCredito = tarjetaDeCredito;
-    }
-
+    
+    /*public void insertarfechas(int a,int b,int c,int d,int e,int f){
+        Scanner input=new Scanner(System.in);
+        Reserva r=new Reserva();
+        
+        r.Registrarentrada(a, b, c);
+        r.Registrarsalida(d, e, f);
+        //this.reserva=r;
+    }*/
 }
