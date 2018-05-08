4bb7fe0437f1c3bd29d10e6cdb796c12179f5cdd
ronald <00401617@uca.edu.sv>
2018-05-08 09:57:10 -0600

    Validacion de datos de entrada

--- src/parcialpoo_2x2/Administrador1.java
+++ src/parcialpoo_2x2/Administrador1.java
@@ -1,87 +1,100 @@
 package parcialpoo_2x2;
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Administrador1 {
 
-    static void nuevaReservacion() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    Scanner sc;
+
+    public Administrador1() {
+        sc = new Scanner(System.in);
+
     }
 
-    public Administrador1(ArrayList<Piso> Pisos) {
-        //   Habitaciones = new ArrayList<>();
-
-        //  this.Habitaciones = Habitaciones;
-    }
-
-    public void add() {
-        Habitacion habitacion = new Habitacion();
-
-        habitacion.add(habitacion);
-
-        Scanner leer = new Scanner(System.in);
-
-        System.out.println("Ingrese tipo de habitacion");
-        System.out.print(": ");
-        habitacion.setTipo(leer.nextLine());
-
-        System.out.println("Ingrese precio");
-        System.out.print(": ");
-        habitacion.setPrecio(leer.nextFloat());
-
-        System.out.println("Ingrese el estado");
-        System.out.print(": ");
-        habitacion.setEstado(leer.nextInt(0));
-    }
-
-    /*public void add(Habitacion habitacion) throws Exception {
-        if (habitacion != null) {
-            if (!Habitacion.contains(habitacion)) {
-                Habitacion.add (habitacion);
+    
+    public String getNombre(String reservar) {
+        String Nombre = "";
+        while (Nombre.equals("")) {
+            System.out.println(reservar);
+            Nombre = sc.nextLine();
+            if (!Nombre.matches("^[A-Za-z ]*$")) {
+                Nombre="";    
             }
-Exception e = new Exception("No se permiten datos duplicados");
-            throw e;
-        } else {
-            throw new Exception("No se puede agregar un estudiante nulo");
         }
-        
+        return Nombre;
     }
-    
-    public void mostrar() {
-        for ( Piso e : Pisos) {
-            System.out.println(e.toString());
-        }
-    }
-    
-    public void eliminar(){
-ArrayList<Piso>lista=new ArrayList<>();
-//Metodo remove, Elimina elementos de nuestra lista mediante indice
 
-lista.remove(Piso.Identificador); 
-//Elimino el elemento si es fila 1, 2 dependiendo de posicion
-        System.out.println("Remove");
-        System.out.println(lista.toString());
+    public int getDui(String reservar) {
+        String DUI = "";
+        while (DUI.equals("")) {
+            System.out.println(reservar);
+            DUI = sc.nextLine();
+            if (!DUI.matches("^[0-9]{8}")) {//cada digito entre 1 y 9, y dui de ocho cifras
+            DUI="";
+            }
+        }
+        return Integer.parseInt(DUI);
     }
-    
-   
-   
-     public void cancelar(){
-         
-     }
-     public void modificar(){
-         
-     }
-     
-    public void habilitar(){
+
+    public String getEstado(String reservar) {
+        String est = "";
+        while (est.equals("")) {
+            System.out.println(reservar);
+            est = sc.nextLine();
+            if (!est.matches("^[A-Za-z]*$")) {//cada digito entre 1 y 9, y ultimod 4 digitos de la tarjeta                       
+            est="";
+            }
+        }
+        return est;
+    }
+
+    public int getNumeroHabitacion(String reservar) {
+        String n = "";
+        while (n.equals("")) {
+            System.out.println(reservar);
+            n = sc.nextLine();
+            if (!n.matches("[1-9]{1}")) {//cada digito entre 1 y 9,para habitaciones de dos cifras                     
+            n="";
+            }
+            
+        }
+        return Integer.parseInt(n);
+    }
+
+    public String getPiso(String reservar) {
+        String Piso = "";
+        while (Piso.equals("")) {
+            System.out.println(reservar);
+            Piso = sc.nextLine();
+            if (!Piso.matches("^[A-Za-z]*$")) {
+            Piso="";
+            }
+        }
+        return Piso;
+    }
+    }
+/*
+    public static void habilitar(String i) {
+      ReservaDeHotel.eliminarHabitacion(i);
         System.out.println("La habitacion esta habilitada");
-        
+
     }
-    public void deshabilitar(){
+
+    public static void deshabilitar(String piso,String numerodehabitacio,String estado) {
+        ReservaDeHotel.agregarHabitacion(piso, numerodehabitacio, estado);
         System.out.println("La habitacion esta deshabilitada");
-        
+
     }
-   
-    
-      */  
+
+    public void cambiarPrecio() {
+        ReservaDeHotel.cambiarprecioshabitaciones();
+
     }
+
+    public void mostrarHabitaciones() {
+        ReservaDeHotel.mostrar();
+    }
+     
+    }*/
